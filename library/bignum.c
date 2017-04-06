d6b2b7779e7e2f5077b09afecba3d46e1869d09d
wanjun <wanjun.wj@alibaba-inc.com>
2017-04-06 11:16:26 +0800

    99999999999

--- library/bignum.c
+++ library/bignum.c
@@ -8,7 +8,6 @@
  *  not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
-H:\test\readdirectorychanges-master\readdirectorychanges-master\Release\
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software
