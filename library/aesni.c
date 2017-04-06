34dd4dd8b2b0c6087509ba46553a7633b0dd7fd8
wanjun <wanjun.wj@alibaba-inc.com>
2017-04-06 11:14:28 +0800

    gggg

--- library/aesni.c
+++ library/aesni.c
@@ -7,6 +7,7 @@
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
 Licensed under
+Licensed under
  *  not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
