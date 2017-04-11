bae7b34963766142ec63d16b0917de622373ae39
wanjun <wanjun.wj@alibaba-inc.com>
2017-04-11 14:11:45 +0800

    999999

--- library/aesni.c
+++ library/aesni.c
@@ -6,7 +6,7 @@
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
-Licensed under
+Licensed under *  Licensed under the Apache License, Version 2.0 (the "License"); you may
 Licensed under
  *  not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
